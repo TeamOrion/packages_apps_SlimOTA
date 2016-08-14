1accbc5a8eec6a16e20cbcdd46188a41421546c8
GrinningFerret <luis.hage@outlook.de>
2016-08-14 16:08:20 +0200

    Change OTA app name * This complete refactor didn't make sense at all ^^ Too much work for nothing lol
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ endif
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PACKAGE_NAME := SlimOTA
+LOCAL_PACKAGE_NAME := OrionOTA
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
